8099c821fe1cc38704182996659231694b3f9c0f
Syed Abbubakar <syed8040@gmail.com>
2024-03-20 03:09:18 +0530

    add pre installation

--- api/index.js
+++ api/index.js
@@ -1,16 +1,19 @@
 import express from "express";
-// import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/userRoute.js";
 import authRouter from "./routes/authRoute.js";
 import listingRouter from "./routes/listingRoute.js";
-// import bodyParser from "body-parser";
 import { connectDB } from "./db/connectDB.js";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 3000
+const MONGO_URL = process.env.MONGO_URI + process.env.MONGO_NAME
+
+const _dirname = path.resolve()
 
 app.use(express.json());
 
@@ -19,6 +22,12 @@ app.use(cookieParser());
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
+
+app.use(express.static(path.join(__dirname, '/client/dist')))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(_dirname, 'client', 'dist', 'index.html'))
+})
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
@@ -30,10 +39,9 @@ app.use((err, req, res, next) => {
   });
 });
 
-const MONGO_URL = process.env.MONGO_URI + process.env.MONGO_NAME
-
 connectDB(MONGO_URL)
-app.listen(3000, () => {
+
+app.listen(PORT, () => {
   console.log(`Server is running on port 3000!`);
 });
 
